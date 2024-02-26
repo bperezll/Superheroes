ce7adb3f77f2085741da31491ce51ac0967c4522
bperezll <borja.perez.llano@gmail.com>
2024-02-26 13:43:47 +0100

    Implemented Retrofit and Picasso Added View Binding Initial API connection done

---
+++ app/src/main/java/com/example/superheroes/activities/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.superheroes.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.superheroes.adapters.SuperheroAdapter
+import com.example.superheroes.data.SuperheroesResponse
+import com.example.superheroes.data.SuperheroesServiceApi
+import com.example.superheroes.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding // View Binding declaration
+
+    private lateinit var adapter: SuperheroAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main) Not necessary with View Binding
+
+        // View binding initialization
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Accessing layout elements
+        binding.searchButton.setOnClickListener {
+            val searchText = binding.searchEditText.text.toString()
+            searchSuperheroes(searchText)
+        }
+
+        adapter = SuperheroAdapter()
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    // Search superheroes from an API using Retrofit
+    private fun searchSuperheroes(query: String) {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service: SuperheroesServiceApi = retrofit.create(SuperheroesServiceApi::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response: Response<SuperheroesResponse> = service.searchByName(query)
+
+            runOnUiThread {
+                if (response != null) {
+                    Log.i("HTTP", "Respuesta correcta")
+                    Log.i("HTTP", "Respuesta ${response.body()?.response}")
+                    Log.i("HTTP", "Respuesta ${response.body()?.results?.first()?.name}")
+                    adapter.updateItems(response.body()?.results)
+
+                } else {
+                    Log.i("HTTP", "Respuesta erronea")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/superheroes/adapters/SuperheroAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.superheroes.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.superheroes.data.Superhero
+import com.example.superheroes.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
+
+class SuperheroAdapter(private var items:List<Superhero> = listOf()) : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemSuperheroBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.render(items[position])
+        //holder.itemView.setOnClickListener { onClickListener(position) }
+    }
+
+    fun updateItems(results: List<Superhero>?) {
+        items = results!!
+        notifyDataSetChanged()
+    }
+}
+
+class ViewHolder(val binding: ItemSuperheroBinding) :RecyclerView.ViewHolder(binding.root) {
+
+    fun render(superhero:Superhero) {
+        binding.nameTextView.text = superhero.name
+        Picasso.get().load(superhero.image.url).into(binding.photoImageView)
+    }
+}
