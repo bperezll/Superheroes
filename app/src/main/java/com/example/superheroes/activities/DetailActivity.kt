8daebdfa7e3ba219e09ca57e77cdf03d187c9e14
bperezll <borja.perez.llano@gmail.com>
2024-02-28 09:31:43 +0100

    Function findSuperheroById done

--- app/src/main/java/com/example/superheroes/activities/DetailActivity.kt
+++ app/src/main/java/com/example/superheroes/activities/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.superheroes.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.superheroes.R
 import com.example.superheroes.adapters.SuperheroAdapter
 import com.example.superheroes.data.Superhero
@@ -10,6 +11,11 @@ import com.example.superheroes.data.SuperheroesServiceApi
 import com.example.superheroes.databinding.ActivityDetailBinding
 import com.example.superheroes.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailActivity : AppCompatActivity() {
 
@@ -40,23 +46,45 @@ class DetailActivity : AppCompatActivity() {
 
         // Get the ID of every superhero, only intent needed
 
-        superheroId = intent.getStringExtra(superhero.id)
+        superheroId = intent.getStringExtra(EXTRA_ID)
         //superhero = Superhero(superheroId!!)
         //detailNavigation = HoroscopeProvider().getHoroscopeIndex(horoscope)
 
         // Display superhero image
 
-        val image = intent.getStringExtra(superhero.image.url)
-        val name = intent.getStringExtra(superhero.name)
+        val image = intent.getStringExtra(EXTRA_IMAGE)
+        val name = intent.getStringExtra(EXTRA_NAME)
 
         Picasso.get().load(image).into(binding.superheroPhotoImageView)
         binding.superheroNameTextView.text = name
 
-        findSuperheroById(superheroId)
+        findSuperheroById(superheroId!!)
 
     }
 
-    private fun findSuperheroById(superheroId: String?) {
+    private fun findSuperheroById(superheroId: String) {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+        val service: SuperheroesServiceApi = retrofit.create(SuperheroesServiceApi::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Llamada en segundo plano
+            val response = service.findById(superheroId)
+
+            runOnUiThread {
+                // Modificar UI
+                //binding.progress.visibility = View.GONE
+                if (response.body() != null) {
+                    Log.i("HTTP", "respuesta correcta :)")
+                    superhero = response.body()!!
+                    binding.superheroNameTextView.text = superhero.name
+                } else {
+                    Log.i("HTTP", "respuesta erronea :(")
+                }
+            }
+        }
     }
 }
